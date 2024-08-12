d6dcbf329b80ec21fb3c0da37b56f98da6ac22e6
GrigorNurijanyan <grigornurijanyan@gmail.com>
2024-08-12 15:37:49 -0400

    deplopyment process 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  pageExtensions: ["workList.js", "skillsList.js", "timelineEvents.js"],
+};
 
 export default nextConfig;
