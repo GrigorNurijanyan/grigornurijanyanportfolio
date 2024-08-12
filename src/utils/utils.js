ba3d0dd1832e9daafa79fcfd57fb27681a457674
GrigorNurijanyan <grigornurijanyan@gmail.com>
2024-08-12 15:29:34 -0400

    deplopyment process 2

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,5 @@
 export const baseURL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:3000"
-    : "https://master.d2lmhr85nshptq.amplifyapp.com";
+  // window.location.hostname === "localhost"
+  //   ? "http://localhost:3000"
+  //   :
+  "https://master.d2lmhr85nshptq.amplifyapp.com";
